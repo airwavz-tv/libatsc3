4667cda1e082b152aa7fccb03d50a0fbe5d8b667
John B Casey <49921236+awzjcasey@users.noreply.github.com>
2020-05-06 22:48:24 -0400

    Change package and modules names to begin with com.airwavz

---
+++ android/sample_app_airwavz/app/src/main/java/com/airwavz/libatsc3/media/sync/mmt/MmtCodecContent.java
@@ -0,0 +1,9 @@
+package com.airwavz.libatsc3.media.sync.mmt;
+
+class MmtCodecContent {
+
+    public Long first_codec_mfu_presentation_time = null;
+    public Boolean first_mfu_presentation_ready_for_emission = false;
+    public Long last_codec_mfu_presentation_time = null;
+
+}
